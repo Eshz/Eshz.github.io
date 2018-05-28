2aa773fd67cde2f714ca2e8ea2217f0b8373e3da
Eshz <39707797+Eshz@users.noreply.github.com>
2018-05-28 20:46:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function changeBackground(bg1) { 
+	document.bgColor = "#ffffff";
+  document.getElementById("typeit").style.color="#212121";
+  document.getElementById("typeit").style.display="block";
+  document.getElementById("box2").style.display="none";
+}
+  
+ function changeBackground2(bg2) { 
+	document.bgColor = "#ffeb3b";
+  document.getElementById("typeit").style.color="#ffffff";
+  document.getElementById("typeit").style.display="block";
+  document.getElementById("box2").style.display="none";
+ }
+
+ function changeSpec(btn5) { 
+  document.getElementById("typeit").style.display="none";
+  document.getElementById("box2").style.display="block";
+
+ }
